a2c618bdc846632e98137cac650210c17393b7a1
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-04-29 12:44:22 +0200

    need another url field

--- parladata/models.py
+++ parladata/models.py
@@ -690,7 +690,8 @@ class Vote(Timestampable, Taggable, models.Model):
                               help_text='The result of the vote')
 
     epa = models.CharField(blank=True, null=True, max_length=255, help_text='EPA number')
-    gov_url = models.CharField(blank=True, null=True, max_length=515, help_text='gov url for this vote')
+    epa_url = models.CharField(blank=True, null=True, max_length=515, help_text='gov url for this vote')
+    document_url = models.CharField(blank=True, null=True, max_length=515, help_text='"document" url for this vote')
 
 
 class Count(Timestampable, models.Model):
