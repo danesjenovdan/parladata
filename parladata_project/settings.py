f679e0f4dba8729d9aa925262e21737f7f7174a6
Tomaz <tomazkunst@gmail.com>
2016-05-23 10:14:31 +0200

    set >use time zone to false<

--- parladata_project/settings.py
+++ parladata_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import raven
+from datetime import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -104,7 +105,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
