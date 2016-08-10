345bafee3008379b96b7fe9e912bb7c322a34f11
muki <filipdobranic@gmail.com>
2016-08-10 22:24:08 +0200

    stupid is what stupid does

--- parlasearch/export.py
+++ parlasearch/export.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from parladata.models import Speech
+from parladata.models import Speech, Session
 
 def export():
 
@@ -33,4 +33,42 @@ def export():
 
         i = i + 1
 
+    return 1
+
+def getSessionContent(session):
+    
+    megastring = u''
+    
+    for speech in session.speech_set.all():
+        megastring = megastring + ' ' + speech.content
+    
+    return megastring
+
+def exportSessions():
+    
+    sessions = Session.objects.all()
+    
+    i = 0
+    
+    for session in sessions:
+        output = [{
+            'id': 's' + str(session.id),
+            'org_i': session.organization.id,
+            'datetime_dt': session.start_time.isoformat(),
+            'content_t': getSessionContent(session),
+            'tip_t': 'burek'
+        }]
+        
+        output = json.dumps(output)
+        
+        if i%100 == 0:
+            r = requests.post('http://127.0.0.1:8983/solr/knedl/update?commit=true', data=output, headers={'Content-Type': 'application/json'})
+
+            print r.text
+
+        else:
+             r = requests.post('http://127.0.0.1:8983/solr/knedl/update', data=output, headers={'Content-Type': 'application/json'})
+
+        i = i + 1
+    
     return 1
