6abc18cb5fcc6394f0f4acb7e50ed5617fb1b5d6
Tomaž Kunst <tomazkunst@gmail.com>
2016-09-06 18:25:22 +0200

    fix empty posts of memberships

--- parladata/utils.py
+++ parladata/utils.py
@@ -433,15 +433,20 @@ def parserChecker(request):
 
 
 def postMembersFixer():
-    for member in Membership.objects.all()
+    for member in Membership.objects.all():
         
-        posts = member.memberships.all().order_by("start_time").exclude(start_time=None)
+        posts = member.memberships.all().order_by("start_time")
         start_time = member.start_time
         firstpost = member.memberships.filter(start_time=start_time)
         if not posts:
-            #Post(start_time=member.start_time, end_time=member.end_time, membership=member, role="član", label="cl", tags=" ").save()
-            print "Dodajanje novega psota"
+            Post(start_time=member.start_time, end_time=member.end_time, membership=member, role="član", label="cl", organization=member.organization).save()
+            print "Dodajanje novega psota", member
         elif firstpost:
+	    posts = posts.exclude(start_time=None)
+	    if posts.count()==0:
+		if firstpost[0].end_time != member.end_time:
+		    print "sam post z None starttimem je in endtime se ne matcha z membershipom"
+		continue
             #There is a post which start on membership start time
             if posts[0] == firstpost:
                 temp_start = posts[0].start_time
@@ -453,5 +458,5 @@ def postMembersFixer():
                 if list(posts)[-1].end_time != membership.end_time:
                     print "konc zadnega posta in membership_end nista ista"
         else:
-            print "start membership:", membership.start_time, "start post:", posts[0].start_time
+            print member.person.name, "start membership:", member.start_time, "start post_prvi:", posts[0].start_time, "start post_zadn:", list(posts)[-1].start_time
             
