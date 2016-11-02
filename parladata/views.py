36e25778b19172858afbc3a7ac03e36fbbbe3902
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-11-02 11:27:33 +0100

    added start/end time to getAllPGsExt output

--- parladata/views.py
+++ parladata/views.py
@@ -444,7 +444,7 @@ def getAllPGs(request):
 
 def getAllPGsExt(request):
     parliamentary_group = Organization.objects.filter(classification="poslanska skupina")
-    data = {pg.id: {'name': pg.name, 'acronym': pg.acronym} for pg in parliamentary_group}
+    data = {pg.id: {'name': pg.name, 'acronym': pg.acronym, 'founded': pg.founding_date, 'disbanded': pg.dissolution_date} for pg in parliamentary_group}
     return JsonResponse(data, safe=False)
 
 def getAllOrganizations(requests):
