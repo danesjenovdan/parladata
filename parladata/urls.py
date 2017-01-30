ad8d3ed9a291feecb9fe00e2bbeb24d1c4469450
T0mTom <juric.tomaz@gmail.com>
2017-01-30 23:36:51 +0100

    more refactoring

--- parladata/urls.py
+++ parladata/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from parladata.views import *
 from .utils import getMembershipDuplications, membersFlowInOrg, postMembersFixer, membersFlowInPGs, membersFlowInDZ
 
@@ -6,131 +6,137 @@ from parladata.admin import PersonAutocomplete, PostAutocomplete, MembershipAuto
 
 urlpatterns = patterns('',
 
-    #autocomplete urls
+    # autocomplete urls
     url(r'^person-autocomplete/$', PersonAutocomplete.as_view(), name='person-autocomplete'),
     url(r'^membership-autocomplete/$', MembershipAutocomplete.as_view(), name='membership-autocomplete'),
     url(r'^post-autocomplete/$', PostAutocomplete.as_view(), name='post-autocomplete'),
 
+    # MPs URLs with and without dates
     url(r'^getActivity/(?P<person_id>\d+)', getActivity),
+
     url(r'^getMPs/(?P<date_>[\w].+)', getMPs),
     url(r'^getMPs', getMPs),
 
     url(r'^getMPStatic/(?P<person_id>\d+)/(?P<date_>[\w].+)', getMPStatic),
     url(r'^getMPStatic/(?P<person_id>\d+)', getMPStatic),
-    url(r'^getSessions/(?P<date_>[\w].+)', getSessions),
-    url(r'^getSessions/', getSessions),
-    url(r'^getSessionsOfOrg/(?P<org_id>\d+)/(?P<date_>[\w].+)', getSessionsOfOrg),
-    url(r'^getSessionsOfOrg/(?P<org_id>\d+)', getSessionsOfOrg),
+
     url(r'^getNumberOfMPAttendedSessions/(?P<person_id>\d+)', getNumberOfMPAttendedSessions),
     url(r'^getNumberOfAllMPAttendedSessions/(?P<date_>[\w].+)', getNumberOfAllMPAttendedSessions),
+
     url(r'^getSpeechesOfMP/(?P<person_id>\d+)/(?P<date_>[\w].+)', getSpeechesOfMP),
     url(r'^getSpeechesOfMP/(?P<person_id>\d+)', getSpeechesOfMP),
+
     url(r'^getSpeechesOfMPbyDate/(?P<person_id>\d+)/(?P<date_>[\w].+)', getSpeechesOfMPbyDate),
-    url(r'^getAllSpeeches/(?P<date_>[\w].+)', getAllSpeeches),
-    url(r'^getAllSpeeches', getAllSpeeches),
+
     url(r'^getAllMPsSpeeches/(?P<date_>[\w].+)', getAllSpeechesOfMPs),
     url(r'^getAllMPsSpeeches', getAllSpeechesOfMPs),
-
-    url(r'^getVotes/(?P<date_>[\w].+)', getVotes),
-    url(r'^getVotes/', getVotes),
 
     url(r'^getSpeeches/(?P<person_id>\d+)/(?P<date_>[\w].+)', getSpeeches),
     url(r'^getSpeeches/(?P<person_id>\d+)', getSpeeches),
 
     url(r'^getMPSpeechesIDs/(?P<person_id>\d+)/(?P<date_>[\w].+)', getSpeechesIDs),
-    url(r'^getPGsSpeechesIDs/(?P<org_id>\d+)/(?P<date_>[\w].+)', getPGsSpeechesIDs),
+
+    url(r'^getAllTimeMPs/(?P<date_>[\w].+)', getAllTimeMPs),
+    url(r'^getAllTimeMPs', getAllTimeMPs),
 
     url(r'^getSpeechesInRange/(?P<person_id>\d+)/(?P<date_from>[\w].+)/(?P<date_to>[\w].+)', getSpeechesInRange),
-
-    url(r'^getMembersOfPGs/',getMembersOfPGs),
-    url(r'^getCoalitionPGs/',getCoalitionPGs),
     url(r'^getMPParty/(?P<person_id>\d+)', getMPParty),
-    url(r'^getNumberOfSeatsOfPG/(?P<pg_id>\d+)',getNumberOfSeatsOfPG),
+    url(r'^getAllPeople/', getAllPeople),
 
-    url(r'^getBasicInfOfPG/(?P<pg_id>\d+)/(?P<date_>[\w].+)',getBasicInfOfPG),
-    url(r'^getBasicInfOfPG/(?P<pg_id>\d+)',getBasicInfOfPG),
-    url(r'^getAllPGs/(?P<date_>[\w].+)',getAllPGs),
-    url(r'^getAllPGs/',getAllPGs),
-    url(r'^getAllPGsExt/',getAllPGsExt),
-    url(r'^getAllOrganizations/',getAllOrganizations),
-    url(r'^getAllPeople/',getAllPeople),
-
-    url(r'^getAllBallots/(?P<date_>[\w].+)',getAllBallots),
-    url(r'^getAllBallots/',getAllBallots),
-
-    url(r'^getAllVotes/(?P<date_>[\w].+)',getAllVotes),
-    url(r'^getAllVotes/',getAllVotes),
-
-    url(r'^motionOfSession/(?P<id_se>\d+)',motionOfSession),
-    url(r'^getVotesOfMotion/(?P<motion_id>\d+)',getVotesOfMotion),
-    url(r'^getVotesOfSession/(?P<id_se>\d+)',getVotesOfSession),
-
-    url(r'^getNumberOfPersonsSessions/(?P<person_id>\d+)/(?P<date_>[\w].+)', getNumberOfPersonsSessions),
-    url(r'^getNumberOfPersonsSessions/(?P<person_id>\d+)', getNumberOfPersonsSessions),
-
-    url(r'^getNumberOfFormalSpeeches/(?P<person_id>\d+)', getNumberOfFormalSpeeches),
     url(r'^getExtendedSpeechesOfMP/(?P<person_id>\d+)', getExtendedSpeechesOfMP),
-    url(r'^getTaggedVotes/(?P<person_id>\d+)', getTaggedVotes),
 
-    url(r'^getMembersOfPGsOnDate/(?P<date_>[\w].+)',getMembersOfPGsOnDate),
-    url(r'^getMembersOfPGsOnDate',getMembersOfPGsOnDate),
+    url(r'^getPersonData/(?P<person_id>\d+)', getPersonData),
 
-    url(r'^getMembersOfPGsRanges/(?P<date_>[\w].+)',getMembersOfPGsRanges),
-    url(r'^getMembersOfPGsRanges',getMembersOfPGsRanges),
-    url(r'^getMembersOfPGRanges/(?P<org_id>\d+)/(?P<date_>[\w].+)',getMembersOfPGRanges),
-    url(r'^getMembersOfPGRanges/(?P<org_id>\d+)',getMembersOfPGRanges),
-
-    url(r'^getMembersOfOrgsRanges/(?P<org_id>\d+)/(?P<date_>[\w].+)',getMembersOfOrgsRanges),
-    url(r'^getMembersOfOrgsRanges/(?P<org_id>\d+)',getMembersOfOrgsRanges),
-
-    url(r'^getMembershipsOfMember/(?P<person_id>\d+)/(?P<date>[\w].+|)',getMembershipsOfMember),
-    url(r'^getAllTimeMemberships',getAllTimeMemberships),
-    url(r'^getAllTimeMPs/(?P<date_>[\w].+)',getAllTimeMPs),
-    url(r'^getAllTimeMPs',getAllTimeMPs),
-    url(r'^getOrganizatonByClassification',getOrganizatonByClassification),
-    url(r'^getOrganizationRolesAndMembers/(?P<org_id>\d+)/(?P<date_>[\w].+)',getOrganizationRolesAndMembers),
-    url(r'^getOrganizationRolesAndMembers/(?P<org_id>\d+)',getOrganizationRolesAndMembers),
-
-    url(r'^getTags', getTags),
-
-    url(r'^getDistricts', getDistricts),
-
-    url(r'^getSpeechData/(?P<speech_id>\d+)',getSpeechData),
-    url(r'^getResultOfMotion/(?P<motion_id>\d+)',getResultOfMotion),
-
-    url(r'^getPersonData/(?P<person_id>\d+)',getPersonData),
-
-    url(r'^isSpeechOnDay/(?P<date_>[\w].+)', isSpeechOnDay),
-    url(r'^isSpeechOnDay/', isSpeechOnDay),
-
-    url(r'^isVoteOnDay/(?P<date_>[\w].+)', isVoteOnDay),
-    url(r'^isVoteOnDay/', isVoteOnDay),
+    url(r'^getMembershipsOfMember/(?P<person_id>\d+)/(?P<date>[\w].+|)', getMembershipsOfMember),
 
     url(r'^getMembersWithFuction/', getMembersWithFuction),
 
-    url(r'^getDocumentOfMotion/(?P<motion_id>[\w].+)', getDocumentOfMotion),
+    url(r'^getNumberOfFormalSpeeches/(?P<person_id>\d+)', getNumberOfFormalSpeeches),
+    url(r'^getTaggedVotes/(?P<person_id>\d+)', getTaggedVotes),
+
+    url(r'^getNumberOfPersonsSessions/(?P<person_id>\d+)/(?P<date_>[\w].+)', getNumberOfPersonsSessions),
+    url(r'^getNumberOfPersonsSessions/(?P<person_id>\d+)', getNumberOfPersonsSessions),
 
     url(r'^getAllQuestions/(?P<date_>[\w].+)', getAllQuestions),
     url(r'^getAllQuestions/', getAllQuestions),
     url(r'^getAllChangesAfter/(?P<datetime_>[\w].+)', getAllChangesAfter),
 
+    url(r'^getDistricts', getDistricts),
+    url(r'^getAllTimeMemberships', getAllTimeMemberships),
+
+    # PGs URLs with and without dates
+    url(r'^getMembersOfPGs/', getMembersOfPGs),
+    url(r'^getCoalitionPGs/', getCoalitionPGs),
+    url(r'^getNumberOfSeatsOfPG/(?P<pg_id>\d+)', getNumberOfSeatsOfPG),
+
+    url(r'^getBasicInfOfPG/(?P<pg_id>\d+)/(?P<date_>[\w].+)', getBasicInfOfPG),
+    url(r'^getBasicInfOfPG/(?P<pg_id>\d+)', getBasicInfOfPG),
+
+    url(r'^getAllPGs/(?P<date_>[\w].+)', getAllPGs),
+    url(r'^getAllPGs/', getAllPGs),
+
+    url(r'^getAllPGsExt/', getAllPGsExt),
+    url(r'^getAllOrganizations/', getAllOrganizations),
+
+    url(r'^getMembersOfPGsOnDate/(?P<date_>[\w].+)', getMembersOfPGsOnDate),
+    url(r'^getMembersOfPGsOnDate', getMembersOfPGsOnDate),
+
+    url(r'^getMembersOfPGsRanges/(?P<date_>[\w].+)', getMembersOfPGsRanges),
+    url(r'^getMembersOfPGsRanges', getMembersOfPGsRanges),
+
+    url(r'^getMembersOfPGRanges/(?P<org_id>\d+)/(?P<date_>[\w].+)', getMembersOfPGRanges),
+    url(r'^getMembersOfPGRanges/(?P<org_id>\d+)', getMembersOfPGRanges),
+
+    url(r'^getMembersOfOrgsRanges/(?P<org_id>\d+)/(?P<date_>[\w].+)', getMembersOfOrgsRanges),
+    url(r'^getMembersOfOrgsRanges/(?P<org_id>\d+)', getMembersOfOrgsRanges),
+
+    url(r'^getOrganizatonByClassification', getOrganizatonByClassification),
+    url(r'^getOrganizationRolesAndMembers/(?P<org_id>\d+)/(?P<date_>[\w].+)', getOrganizationRolesAndMembers),
+    url(r'^getOrganizationRolesAndMembers/(?P<org_id>\d+)', getOrganizationRolesAndMembers),
+
+    url(r'^getPGsSpeechesIDs/(?P<org_id>\d+)/(?P<date_>[\w].+)', getPGsSpeechesIDs),
+
+    # Votes and Motion URLs
+    url(r'^getAllVotes/(?P<date_>[\w].+)', getAllVotes),
+    url(r'^getAllVotes/', getAllVotes),
+    url(r'^getDocumentOfMotion/(?P<motion_id>[\w].+)', getDocumentOfMotion),
+    url(r'^isVoteOnDay/(?P<date_>[\w].+)', isVoteOnDay),
+    url(r'^isVoteOnDay/', isVoteOnDay),
+    url(r'^getTags', getTags),
+    url(r'^getVotes/(?P<date_>[\w].+)', getVotes),
+    url(r'^getVotes/', getVotes),
+    url(r'^motionOfSession/(?P<id_se>\d+)', motionOfSession),
+    url(r'^getVotesOfMotion/(?P<motion_id>\d+)', getVotesOfMotion),
+    url(r'^getVotesOfSession/(?P<id_se>\d+)', getVotesOfSession),
+
+    # Ballots URLs
+    url(r'^getAllBallots/(?P<date_>[\w].+)', getAllBallots),
+    url(r'^getAllBallots/', getAllBallots),
+
+    # Sessions URLs
+    url(r'^getSessions/(?P<date_>[\w].+)', getSessions),
+    url(r'^getSessions/', getSessions),
+    url(r'^getSessionsOfOrg/(?P<org_id>\d+)/(?P<date_>[\w].+)', getSessionsOfOrg),
+    url(r'^getSessionsOfOrg/(?P<org_id>\d+)', getSessionsOfOrg),
+
+    # Speech URLs
+    url(r'^getAllSpeeches/(?P<date_>[\w].+)', getAllSpeeches),
+    url(r'^getAllSpeeches', getAllSpeeches),
+    url(r'^isSpeechOnDay/(?P<date_>[\w].+)', isSpeechOnDay),
+    url(r'^isSpeechOnDay/', isSpeechOnDay),
+    url(r'^getSpeechData/(?P<speech_id>\d+)', getSpeechData),
 
     # POST save url's for parser
     url(r'^addQuestion/', addQuestion),
 
-
-
-    #debug helpers
+    # debug helpers
     url(r'^getMembershipDuplications', getMembershipDuplications),
     url(r'^parserChecker', parserChecker),
     url(r'^postMembersFixer', postMembersFixer),
     url(r'^membersFlowInOrg', membersFlowInOrg),
     url(r'^membersFlowInPGs', membersFlowInPGs),
     url(r'^membersFlowInDZ', membersFlowInDZ),
-
-    url(r'^sejee/(?P<date_>[\w].+)',checkSessions),
-
-
+    url(r'^checkSessions/(?P<date_>[\w].+)', checkSessions),
 
     # MONITORING
     url(r'^monitoring', monitorMe),
