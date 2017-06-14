8c8ee3fc824ff5d3ab2c463b55282b0528ce20f1
Tomaž Kunst <tomazkunst@gmail.com>
2017-06-14 21:47:10 +0200

    * update voterparty of ballots * export amendments

--- sandbox/export.py
+++ sandbox/export.py
@@ -5,6 +5,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding("UTF-8")
 
+PS = ['poslanska skupina']
 
 def dictToCSV(data, file_name):
     with open('exports/' + file_name, 'wb') as csvfile:
@@ -39,3 +40,18 @@ def listToCSV(data, file_name):
                 e_data = [value]
             csvwriter.writerow(e_data)
     return 'saved in exports/' + file_name
+
+
+def getAmendment():
+    data = [['PG', 'status', 'link']]
+    parliamentaryGroups = Organization.objects.filter(classification__in=PS)
+    acronyms = list(set(list(parliamentaryGroups.values_list('acronym', flat=True))))
+    amandmas = Vote.objects.filter(name__icontains='amandma')
+    for acronym in acronyms:
+        temp_amandmas = amandmas.filter(name__icontains="["+acronym).values_list("session_id", "id", "motion__result")
+        pg_amandmas = [list(map(str, i))
+                       for i
+                       in temp_amandmas]
+        for amandma in pg_amandmas:
+            data.append([acronym, amandma[2], 'https://parlameter.si/seja/glasovanje/' + '/'.join(amandma[:2])])
+    listToCSV(data, 'amandmaji.csv')
