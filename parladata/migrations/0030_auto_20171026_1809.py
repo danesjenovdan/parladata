24e7a826a12cc81640c0224728c1b9bc768de833
T0mTom <juric.tomaz@gmail.com>
2017-10-26 19:17:51 +0200

    migracija pf pf

---
+++ parladata/migrations/0030_auto_20171026_1809.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import tinymce.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('parladata', '0029_auto_20171001_1655'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='law',
+            old_name='result',
+            new_name='status',
+        ),
+        migrations.AddField(
+            model_name='law',
+            name='note',
+            field=tinymce.models.HTMLField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='law',
+            name='procedure',
+            field=models.CharField(help_text=b'Procedure of law', max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='law',
+            name='procedure_phase',
+            field=models.CharField(help_text=b'Procedure phase of law', max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='law',
+            name='proposer_text',
+            field=models.CharField(help_text=b'Proposer of law', max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='law',
+            name='type_of_law',
+            field=models.CharField(help_text=b'Type of law', max_length=255, null=True, blank=True),
+        ),
+    ]
