b442c31d1e108363ac4ade16b464251e21a991a5
Tomaž <cofek0@gmail.com>
2018-01-30 11:31:06 +0100

    set docs public

--- parladata_project/urls.py
+++ parladata_project/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     url(r'^v1/', include('parladata.urls', namespace='API')),
     url(r'^v1/tasks/', include('parlasearch.urls')),
     url(r'^v1/sandbox/', include('sandbox.urls')),
-    url(r'^docs/', include_docs_urls(title='My API title'))
+    url(r'^docs/', include_docs_urls(title='My API title', public=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
