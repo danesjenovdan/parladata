be87c07b2fdee5d9b0e61b9dc1c8f63ffaa7b12b
Tomaz <cofek0@gmail.com>
2019-02-20 14:00:26 +0000

    add slolem linking to shell script

--- solr/scripts/linkconf_prod.sh
+++ solr/scripts/linkconf_prod.sh
@@ -1,3 +1,10 @@
 su solr -c "rm /var/solr/data/parlasearch/conf/managed-schema"
 su solr -c "ln -s --force /home/parladaddy/parlasearch/solr/parlasearch-conf/* /var/solr/data/parlasearch/conf/"
 
+# link slolem to common directory
+ln -s --force /home/parladaddy/parlasearch/solr/slolem /opt/solr-slolem
+
+# append to file
+LINE='SOLR_OPTS="$SOLR_OPTS -Djava.library.path=/opt/solr-slolem/bin"'
+FILE=/etc/default/solr.in.sh
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
