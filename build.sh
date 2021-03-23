05eeb9eaa42edf34b9bea6a9a39e125e85c8e2aa
muki <filipdobranic@gmail.com>
2021-03-23 17:00:50 +0100

    annotate pv claim template with gid to allow access

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+TAG=latest
+
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
 # BUILD AND PUBLISH API
-sudo docker build -f api/Dockerfile -t parlasearch:latest api
-sudo docker tag parlasearch:latest rg.fr-par.scw.cloud/djnd/parlasearch:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/parlasearch:latest
+sudo docker build -f api/Dockerfile -t parlasearch:$TAG api
+sudo docker tag parlasearch:$TAG rg.fr-par.scw.cloud/djnd/parlasearch:$TAG
+sudo docker push rg.fr-par.scw.cloud/djnd/parlasearch:$TAG
 
 # BUILD AND PUBLISH SOLR
-sudo docker build -f solr/Dockerfile -t parlasolr:latest solr
-sudo docker tag parlasolr:latest rg.fr-par.scw.cloud/djnd/parlasolr:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/parlasolr:latest
+sudo docker build -f solr/Dockerfile -t parlasolr:$TAG solr
+sudo docker tag parlasolr:$TAG rg.fr-par.scw.cloud/djnd/parlasolr:$TAG
+sudo docker push rg.fr-par.scw.cloud/djnd/parlasolr:$TAG
