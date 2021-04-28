8da48c1b37d3803040f4cfc455ce4c871848dafa
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-04-28 12:06:37 +0200

    update title on docs page

--- parladata_project/urls.py
+++ parladata_project/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^v3/', include(('parladata.urls', 'apiV3'), namespace='APIv3')),
     url(r'^v1/sandbox/', include(('sandbox.urls', 'sandobx'), namespace='sandbox')),
-    url(r'^', include_docs_urls(title='Parladata v1 API', public=True)),
+    url(r'^', include_docs_urls(title='Parladata v3 API', public=True)),
     url(r'^oauth/', include(('oauth2_provider.urls', 'oauth'), namespace='oauth2_provider')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
