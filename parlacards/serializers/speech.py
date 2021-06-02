a92b5da3d066575698e15faab1555614132d95fa
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-06-02 12:29:31 +0200

    add session/speech card endpoint

--- parlacards/serializers/speech.py
+++ parlacards/serializers/speech.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
 
+from parlacards.serializers.session import SessionSerializer
+
 from parlacards.serializers.common import CommonSerializer
 
 class SpeechSerializer(CommonSerializer):
-    pass
+    def get_the_order(self, obj):
+        # obj is the speech
+        if obj.order:
+            return obj.order
+        return obj.id
+    
+    def get_quoted_text(self, obj):
+        return None
+    
+    def get_start_idx(self, obj):
+        return None
+    
+    def get_end_idx(self, obj):
+        return None
+    
+    def get_quote_id(self, obj):
+        return None
+
+    speech_id = serializers.IntegerField(source='id')
+    content = serializers.CharField()
+    session = SessionSerializer()
+    the_order = serializers.SerializerMethodField()
+    quoted_text = serializers.SerializerMethodField()
+    start_idx = serializers.SerializerMethodField()
+    end_idx = serializers.SerializerMethodField()
+    quote_id = serializers.SerializerMethodField()
