b58a868821977d8ca631513f1c0478324c6ce4f0
muki <filipdobranic@gmail.com>
2021-06-08 12:21:34 +0200

    some more data url changes

--- api/config/sample.js
+++ api/config/sample.js
@@ -1,9 +1,9 @@
 module.exports = {
   urls: {
-    cdn: 'https://cdn.parlameter.si/v1/parlassets',
-    analize: 'https://analize.parlameter.si/v1',
-    data: 'https://data.parlameter.si/v1',
-    isci: 'https://isci.parlameter.si',
+    cdn: 'https://cdn.nov.parlameter.si/v1/parlassets',
+    analize: 'https://analize.nov.parlameter.si/v1',
+    data: 'https://data.nov.parlameter.si/v1',
+    isci: 'https://isci.nov.parlameter.si',
     glej: 'http://localhost:3000',
     base: 'http://localhost:3066',
   }
