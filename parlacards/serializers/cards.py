089319cb2dcc6711a726b7e3a55ac6b0e73535c7
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-06-17 13:58:09 +0200

    fully implement poeple/speeches and group/speeches

--- parlacards/serializers/cards.py
+++ parlacards/serializers/cards.py
@@ -378,11 +378,13 @@ class PersonSpeechesCardSerializer(PersonScoreCardSerializer):
     def get_results(self, obj):
         # obj is the person
         solr_params = {
-            'person_id': obj.id,
+            'people_ids': [obj.id],
             'highlight': True
         }
-        if self.context['GET'].get('content', False):
-            solr_params['text_query'] = self.context['GET']['content']
+        if self.context['GET'].get('text', False):
+            solr_params['text_query'] = self.context['GET']['text']
+        if self.context['GET'].get('months', False):
+            solr_params['months'] = self.context['GET']['months'].split(',')
 
         serializer = SpeechSerializer(
             get_speeches_from_solr(**solr_params),
@@ -396,11 +398,15 @@ class GroupSpeechesCardSerializer(GroupScoreCardSerializer):
     def get_results(self, obj):
         # obj is the person
         solr_params = {
-            'group_id': obj.id,
+            'group_ids': [obj.id],
             'highlight': True
         }
-        if self.context['GET'].get('content', False):
-            solr_params['text_query'] = self.context['GET']['content']
+        if self.context['GET'].get('text', False):
+            solr_params['text_query'] = self.context['GET']['text']
+        if self.context['GET'].get('people', False):
+            solr_params['people_ids'] = self.context['GET']['people'].split(',')
+        if self.context['GET'].get('months', False):
+            solr_params['months'] = self.context['GET']['months'].split(',')
 
         serializer = SpeechSerializer(
             get_speeches_from_solr(**solr_params),
