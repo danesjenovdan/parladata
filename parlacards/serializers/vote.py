2cb9b5f53568ba5b3409cb357a40df07e3508d79
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-06-20 12:20:59 +0200

    vote caching v0.1

--- parlacards/serializers/vote.py
+++ parlacards/serializers/vote.py
@@ -1,6 +1,7 @@
-from django.db.models import Count
-
 from collections import Counter
+
+from django.core.cache import cache
+from django.db.models import Count
 
 from rest_framework import serializers
 
@@ -9,15 +10,96 @@ from parladata.models.ballot import Ballot
 
 from parlacards.serializers.session import SessionSerializer
 
-from parlacards.serializers.common import CommonSerializer, CommonPersonSerializer, CommonOrganizationSerializer
+from parlacards.serializers.common import (
+    CommonSerializer,
+    CommonPersonSerializer,
+    CommonOrganizationSerializer,
+    CommonCachableSerializer
+)
 
-class VoteBallotSerializer(CommonSerializer):
+class VoteBallotSerializer(CommonCachableSerializer):
+    def calculate_cache_key(self, instance):
+        # instance is the vote
+        # TODO what if ballots change?
+        vote_timestamp = instance.updated_at
+        person_timestamp = instance.personvoter.updated_at
+        timestamp = max([vote_timestamp, person_timestamp])
+        return f'VoteBallotSerializer_{instance.id}_{instance.personvoter.id}_{timestamp.strftime("%Y-%m-%d-%H-%M-%s")}'
+
     person = CommonPersonSerializer(source='personvoter')
     outlier = serializers.BooleanField()
     option = serializers.CharField()
 
 
-class VoteGroupSerializer(CommonSerializer):
+class VoteGroupSerializer(CommonCachableSerializer):
+    def calculate_cache_key(self, instance):
+        # instance is group
+        # TODO what if ballots change?
+        group_timestamp = instance.updated_at
+        vote_timestamp = self.context['vote'].updated_at
+        timestamp = max([group_timestamp, vote_timestamp])
+        return f'VoteGroupSerializer_{instance.id}_{self.context["vote"].id}_{timestamp.strftime("%Y-%m-%d-%H-%M-%s")}'
+
+    def get_group_ballots(self, group):
+        vote_ballots = Ballot.objects.filter(
+            vote=self.context['vote']
+        )
+
+        group_ballots = vote_ballots.filter(
+            # TODO this should be reworked
+            # we need a special function
+            # that finds all members with voting
+            # rights in the playing field on a given date
+            personvoter__in=group.query_members(self.context['date']),
+        )
+
+        return group_ballots
+    
+    def get_annotated_group_ballots(self, group):
+        annotated_group_ballots = self.get_group_ballots(group).values(
+            'option'
+        ).annotate(
+            option_count=Count('option')
+        ).order_by('-option_count')
+
+        return annotated_group_ballots
+
+    def get_outliers(self, obj):
+        # obj is the group
+        max_option = self.get_annotated_group_ballots(obj).first()['option']
+
+        filtered_group_ballots = self.get_group_ballots(obj).exclude(option__in=['absent', max_option])
+        outliers = [
+            ballot.personvoter for ballot in filtered_group_ballots
+        ]
+
+        serializer = CommonPersonSerializer(
+            outliers,
+            many=True,
+            context=self.context
+        )
+        return serializer.data
+    
+    def get_max(self, obj):
+        # obj is group
+        max_option_percentage = self.get_annotated_group_ballots(obj).first()['option_count'] * 100 / sum(self.get_annotated_group_ballots(obj).values_list('option_count', flat=True))
+        max_option = self.get_annotated_group_ballots(obj).first()['option']
+        return {
+            'max_option_percentage': max_option_percentage,
+            'max_option': max_option
+        }
+    
+    def get_votes(self, obj):
+        # obj is group
+        group_votes_params = {
+            option_sum['option']: option_sum['option_count'] for
+            option_sum in self.get_annotated_group_ballots(obj)
+        }
+        return {
+            key: group_votes_params.get(key, 0) for
+            key in ['absent', 'abstain', 'for', 'against'] # TODO get this from global var
+        }
+
     def get_group(self, obj):
         # obj is group
         serializer = CommonOrganizationSerializer(
@@ -26,10 +108,31 @@ class VoteGroupSerializer(CommonSerializer):
         )
         return serializer.data
 
-    max = serializers.DictField()
-    votes = serializers.DictField()
-    outliers = CommonPersonSerializer(many=True)
+    max = serializers.SerializerMethodField()
+    votes = serializers.SerializerMethodField()
+    outliers = serializers.SerializerMethodField()
     group = serializers.SerializerMethodField()
+
+
+class VoteSumsSerializer(CommonCachableSerializer):
+    def calculate_cache_key(self, instance):
+        # instance is the vote
+        # TODO should the vote change when ballots change?
+        # maybe touch, maybe a better cache key, but this
+        # one is cheap
+        return f'VoteSumsSerializer_{instance.id}_{instance.updated_at.strftime("%Y-%m-%d-%H-%M-%s")}'
+
+    # for is a reserved word FML
+    def to_representation(self, instance):
+        # instance is vote
+        cache_key = self.calculate_cache_key(instance)
+        cached_representation = cache.get(cache_key)
+        if cached_representation:
+            return cached_representation
+
+        representation = instance.get_option_counts()
+        cache.set(cache_key, representation)
+        return representation
 
 
 class VoteSerializer(CommonSerializer):
@@ -113,48 +216,10 @@ class VoteSerializer(CommonSerializer):
 
     def get_groups(self, obj):
         # obj is the vote
-        vote_ballots = Ballot.objects.filter(
-            vote=obj
-        )
-
         groups = obj.motion.session.organization.query_parliamentary_groups(self.context['date'])
-
-        # add data to groups
-        for group in groups:
-            group_ballots = vote_ballots.filter(
-                # TODO this should be reworked
-                # we need a special function
-                # that finds all members with voting
-                # rights in the playing field on a given date
-                personvoter__in=group.query_members(self.context['date']),
-            )
-            
-            annotated_group_ballots = group_ballots.values(
-                'option'
-            ).annotate(
-                option_count=Count('option')
-            ).order_by('-option_count')
-
-            # set group max dict
-            max_option_percentage = annotated_group_ballots.first()['option_count'] * 100 / sum(annotated_group_ballots.values_list('option_count', flat=True))
-            max_option = annotated_group_ballots.first()['option']
-            group.max = {
-                'max_option_percentage': max_option_percentage,
-                'max_option': max_option
-            }
-
-            # set group votes dict
-            group_votes_params = {
-                option_sum['option']: option_sum['option_count'] for option_sum in annotated_group_ballots
-            }
-            group.votes = {
-                key: group_votes_params.get(key, 0) for key in ['absent', 'abstain', 'for', 'against']
-            }
-
-            # set group outliers
-            group.outliers = [
-                ballot.personvoter for ballot in group_ballots.exclude(option__in=['absent', max_option])
-            ]
+    
+        # pass the vote to the serializer
+        self.context['vote'] = obj
         serializer = VoteGroupSerializer(
             groups,
             many=True,
@@ -172,15 +237,14 @@ class VoteSerializer(CommonSerializer):
         return None
     
     def get_all_votes(self, obj):
-        return {
-            'absent': 1.0,
-            'abstain': 0.0,
-            'for': 42.0,
-            'against': 0.0
-        }
+        # obj is the vote
+        serializer = VoteSumsSerializer(
+            obj,
+            context=self.context
+        )
+        return serializer.data
 
-    all_votes = serializers.SerializerMethodField() # TODO make this work
-
+    all_votes = serializers.SerializerMethodField()
     government_sides = serializers.SerializerMethodField() # TODO this is faked
     abstract_visible = serializers.SerializerMethodField() # TODO this is faked
     session = serializers.SerializerMethodField()
@@ -195,85 +259,44 @@ class VoteSerializer(CommonSerializer):
 
 
 class SessionVoteSerializer(CommonSerializer):
-    def get_votes_abstain(self, obj):
-        # obj is the vote
-        return Ballot.objects.filter(
-            vote=obj,
-            option='abstain'
-        ).count()
-    
-    def get_votes_absent(self, obj):
-        # obj is the vote
-        return Ballot.objects.filter(
-            vote=obj,
-            option='absent'
-        ).count()
-    
-    def get_votes_for(self, obj):
-        # obj is the vote
-        return Ballot.objects.filter(
-            vote=obj,
-            option='for'
-        ).count()
-    
-    def get_votes_against(self, obj):
-        # obj is the vote
-        return Ballot.objects.filter(
-            vote=obj,
-            option='against'
-        ).count()
-    
     def get_passed(self, obj):
-        return None
-    
-    def get_session_id(self, obj):
         # obj is the vote
-        return obj.motion.session.id
+        # TODO calculate properly
+        return None
 
     def get_is_outlier(self, obj):
-        # TODO
+        # TODO some day
         return False
     
     def get_has_outliers(self, obj):
-        # TODO
+        # TODO before some day
         return False
     
     def get_has_named_votes(self, obj):
-        # TODO
+        # TODO before some day
         return True
 
     def get_title(self, obj):
-        # TODO
+        # TODO return motion object
+        # and handle it on the front-end
         return obj.motion.title
     
     def get_all_votes(self, obj):
-        # TODO
-        # return  {
-        #     absent
-        #     abstain
-        #     for
-        #     against
-        # }
-        return {
-            'absent': 1.0,
-            'abstain': 0.0,
-            'for': 42.0,
-            'against': 0.0
-        }
+        # obj is the vote
+        serializer = VoteSumsSerializer(
+            obj,
+            context=self.context
+        )
+        return serializer.data
 
     all_votes = serializers.SerializerMethodField()
-    # votes_abstain = serializers.SerializerMethodField()
-    # votes_absent = serializers.SerializerMethodField()
-    # votes_for = serializers.SerializerMethodField()
-    # votes_against = serializers.SerializerMethodField()
     passed = serializers.SerializerMethodField()
-    # session_id = serializers.SerializerMethodField()
     id = serializers.IntegerField()
     is_outlier = serializers.SerializerMethodField()
     has_outliers = serializers.SerializerMethodField()
     has_named_votes = serializers.SerializerMethodField()
     title = serializers.SerializerMethodField()
-
+        
 
 class SpeechVoteSerializer(CommonSerializer):
     result = serializers.SerializerMethodField()
