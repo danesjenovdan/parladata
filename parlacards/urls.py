6cdfbffd98bf87f161f75980b151fb68f1105675
LepkoQQ <lepko.san@gmail.com>
2021-07-06 15:58:05 +0200

    add search usage-by-group api

--- parlacards/urls.py
+++ parlacards/urls.py
@@ -56,4 +56,5 @@ urlpatterns = [
     path('vote/single/', SingleVote.as_view()),
 
     path('search/speeches/', MandateSpeeches.as_view()),
+    path('search/usage-by-group/', MandateUsageByGroup.as_view()),
 ]
