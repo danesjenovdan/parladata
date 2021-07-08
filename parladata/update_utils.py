5ac9211474e322170da6ca9a9ac4c769e2489c06
Tomaz <tomazkunst@gmail.com>
2021-07-08 12:52:03 +0200

    remove unused import

--- parladata/update_utils.py
+++ parladata/update_utils.py
@@ -1,5 +1,4 @@
 from django.db.models import Q
-from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
