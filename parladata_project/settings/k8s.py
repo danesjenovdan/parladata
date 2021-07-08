45e8cf3d984609533604a4c5162aacf7345e9f80
Tomaz <tomazkunst@gmail.com>
2021-07-08 14:43:18 +0200

    forgeten bool

--- parladata_project/settings/k8s.py
+++ parladata_project/settings/k8s.py
@@ -69,8 +69,8 @@ SOLR_URL = env['SOLR_URL']
 
 EMAIL_BACKEND = os.getenv('PARLAMETER_EMAIL_BACKEND', 'django.core.mail.backends.filebased.EmailBackend')
 EMAIL_FILE_PATH = os.getenv('PARLAMETER_EMAIL_FILE_PATH', '/tmp/emails')
-EMAIL_USE_TLS = os.getenv('PARLAMETER_EMAIL_USE_TLS', '')
-EMAIL_USE_SSL = os.getenv('PARLAMETER_EMAIL_USE_SSL', '')
+EMAIL_USE_TLS = bool(os.getenv('PARLAMETER_EMAIL_USE_TLS', ''))
+EMAIL_USE_SSL = bool(os.getenv('PARLAMETER_EMAIL_USE_SSL', ''))
 EMAIL_HOST = os.getenv('PARLAMETER_EMAIL_HOST', 'dummy')
 EMAIL_PORT = int(os.getenv('PARLAMETER_EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.getenv('PARLAMETER_SMTP_USER', 'dummy')
