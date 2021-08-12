d87f642ee67a021c7b3ce13658cc85ee3b756f07
Tomaz <tomazkunst@gmail.com>
2021-08-12 14:54:08 +0200

    fixed tfidf filter and add button for tfidf to people, sessions and groups

--- parlacards/admin.py
+++ parlacards/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django import forms
 from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.conf import settings
+from django.shortcuts import redirect
 
 from parlacards.models import (SessionTfidf, PersonTfidf, GroupTfidf)
 from parladata.models.memberships import PersonMembership
@@ -64,6 +67,8 @@ class SessionTfidfAdmin(admin.ModelAdmin):
     list_per_page = 20
     ordering = ['timestamp__date', '-value']
     list_editable = ('token',)
+    autocomplete_fields = ['session']
+    deleted_session_fk = None
 
     def get_queryset(self, request):
         return SessionTfidf.objects.all().prefetch_related('session')
@@ -81,6 +86,15 @@ class SessionTfidfAdmin(admin.ModelAdmin):
     def delete(self, obj):
         return mark_safe(f'<input onclick="location.href=\'{obj.pk}/delete/\'" type="button" value="Delete" />')
 
+    def delete_view(self, request, object_id, extra_context=None):
+        self.deleted_session_fk = SessionTfidf.objects.get(id=object_id).session_id
+        return super().delete_view(request, object_id, extra_context)
+
+    def response_delete(self, request, obj_display, obj_id):
+        partial_url = reverse('admin:parlacards_sessiontfidf_changelist')
+        url = f'{settings.BASE_URL}{partial_url}?session__id__exact={self.deleted_session_fk}'
+        return redirect(url)
+
     delete.allow_tags = True
     delete.short_description = 'Delete object'
 
@@ -91,6 +105,8 @@ class PersonTfidfAdmin(admin.ModelAdmin):
     list_per_page = 20
     ordering = ['timestamp__date', '-value']
     list_editable = ('token',)
+    autocomplete_fields = ['person']
+    deleted_member_fk = None
 
     def get_changelist_formset(self, request, **kwargs):
         data = super().get_changelist_formset(request, **kwargs)
@@ -101,6 +117,16 @@ class PersonTfidfAdmin(admin.ModelAdmin):
 
     def delete(self, obj):
         return mark_safe(f'<input onclick="location.href=\'{obj.pk}/delete/\'" type="button" value="Delete" />')
+
+    def delete_view(self, request, object_id, extra_context=None):
+        self.deleted_member_fk = PersonTfidf.objects.get(id=object_id).person_id
+        return super().delete_view(request, object_id, extra_context)
+
+    def response_delete(self, request, obj_display, obj_id):
+        partial_url = reverse('admin:parlacards_persontfidf_changelist')
+        url = f'{settings.BASE_URL}{partial_url}?member={self.deleted_member_fk}'
+        return redirect(url)
+
 
     delete.allow_tags = True
     delete.short_description = 'Delete object'
@@ -112,6 +138,8 @@ class GroupTfidfAdmin(admin.ModelAdmin):
     list_per_page = 20
     ordering = ['timestamp__date', '-value']
     list_editable = ('token',)
+    autocomplete_fields = ['group']
+    deleted_group_fk = None
 
     def get_changelist_formset(self, request, **kwargs):
         data = super().get_changelist_formset(request, **kwargs)
@@ -122,6 +150,15 @@ class GroupTfidfAdmin(admin.ModelAdmin):
 
     def delete(self, obj):
         return mark_safe(f'<input onclick="location.href=\'{obj.pk}/delete/\'" type="button" value="Delete" />')
+
+    def delete_view(self, request, object_id, extra_context=None):
+        self.deleted_group_fk = GroupTfidf.objects.get(id=object_id).organization_id
+        return super().delete_view(request, object_id, extra_context)
+
+    def response_delete(self, request, obj_display, obj_id):
+        partial_url = reverse('admin:parlacards_grouptfidf_changelist')
+        url = f'{settings.BASE_URL}{partial_url}?organization={self.deleted_group_fk}'
+        return redirect(url)
 
     delete.allow_tags = True
     delete.short_description = 'Delete object'
