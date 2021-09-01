183e962cffb188ef0847683b065023ac2f5a1086
Tomaz <tomazkunst@gmail.com>
2021-09-01 16:18:54 +0200

    make better vote screen

--- parladata/admin/vote.py
+++ parladata/admin/vote.py
@@ -3,10 +3,12 @@ from django.utils.safestring import mark_safe
 from django.urls import reverse
 from django.conf import settings
 
-from parladata.models import Vote
+from parladata.models import Vote, Ballot
+
+from collections import Counter
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'the_tags', 'add_ballots')
+    list_display = ('id', 'name', 'the_tags', 'get_for', 'get_against', 'get_abstain', 'get_abesnt', 'add_ballots')
 
     list_filter = ('tags',)
     inlines = [
@@ -15,8 +17,8 @@ class VoteAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
     def the_tags(self, obj):
-        return "%s" % (obj.tags.all(), )
-    the_tags.short_description = 'tags'
+        return "%s" % (list(obj.tags.values_list('name', flat=True)), )
+    
 
     def add_ballots(self, obj):
         partial_url = '/admin/parladata/vote/addballots/'
@@ -26,6 +28,27 @@ class VoteAdmin(admin.ModelAdmin):
         else:
             return ''
 
+    def get_for(self, obj):
+        results = dict(Counter(Ballot.objects.filter(vote=obj).values_list("option", flat=True))).get("for", 0)
+        return results
+
+    def get_against(self, obj):
+        results = dict(Counter(Ballot.objects.filter(vote=obj).values_list("option", flat=True))).get("against", 0)
+        return results
+
+    def get_abstain(self, obj):
+        results = dict(Counter(Ballot.objects.filter(vote=obj).values_list("option", flat=True))).get("abstain", 0)
+        return results
+
+    def get_abesnt(self, obj):
+        results = dict(Counter(Ballot.objects.filter(vote=obj).values_list("option", flat=True))).get("absent", 0)
+        return results
+
+    get_for.short_description = 'for'
+    get_against.short_description = 'against'
+    get_abstain.short_description = 'abstain'
+    get_abesnt.short_description = 'absent'
+    the_tags.short_description = 'tags'
     add_ballots.allow_tags = True
     add_ballots.short_description = 'Add ballots'
 
