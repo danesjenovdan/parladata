3f7f13f6f038ceddf5c803079891a6d8b3b36a08
LepkoQQ <lepko.san@gmail.com>
2021-11-15 16:23:41 +0100

    move root org helper to mandate model

--- parlacards/utils.py
+++ parlacards/utils.py
@@ -6,12 +6,3 @@ from icu import Collator, Locale
 
 
 local_collator = Collator.createInstance(Locale(settings.LANGUAGE_CODE))
-
-
-def get_organizations_from_mandate(mandate, date):
-    membership = OrganizationMembership.valid_at(date).filter(mandate=mandate).first()
-    if not membership:
-        raise Exception(f'Root organization membership for this mandate does not exist')
-    playing_field = membership.member
-    root_organization = membership.organization
-    return root_organization, playing_field
