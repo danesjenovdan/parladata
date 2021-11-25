ce7e9b2ef92117d9cedee690ae05e31578079724
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-11-25 13:50:17 +0100

    add script to import database from the cluster

---
+++ import_db.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# EDIT DATABASE NAME TO CHOOSE WHICH ONE YOU WANT
+DATABASE_NAME="parladata"
+
+# DATABASE PASSWORD IS DYNAMICALLY RETRIEVED FROM THE CLUSTER
+DATABASE_PASSWORD=$(kubectl get secret postgresql -n shared -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+
+echo
+echo "PORT FORWARDING"
+nohup kubectl port-forward pod/postgresql-0 54321:5432 --namespace=shared &>/dev/null &
+
+echo
+echo "DUMPING DATABASE TO db.dump"
+PGPASSWORD=$DATABASE_PASSWORD \
+    pg_dump -U postgres \
+        -f db.dump \
+        -d $DATABASE_NAME \
+        -p 54321 \
+        -h localhost
+
+echo
+echo "DROPPING THE DB VOLUME"
+sudo docker-compose down -v db
+sudo docker-compose up -d
+
+echo
+echo "COPYING THE DUMP FILE TO THE CONTAINER"
+sudo docker-compose cp db.dump db:db.dump
+
+echo
+echo "LOADING DB INTO CONTAINER"
+sudo docker-compose exec db psql -U postgres parladata < 'db.dump'
+
+sudo docker-compose down
+echo
+echo "ALL DONE, YOU CAN RUN docker-compose up"
