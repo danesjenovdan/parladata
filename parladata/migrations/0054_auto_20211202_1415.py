b7d70cf8c61154339466a89a201ae0a15d1d930f
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-12-02 15:25:49 +0100

    fix latest migration so it survives a database with data

--- parladata/migrations/0054_auto_20211202_1415.py
+++ parladata/migrations/0054_auto_20211202_1415.py
@@ -3,7 +3,21 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+# added this to clean the current classification values
+def clean_legislation_classifications(apps, schema_editor):
+    # We get the model from the versioned app registry;
+    # if we directly import it, it'll be the wrong version
+    Law = apps.get_model('parladata', 'Law')
+    db_alias = schema_editor.connection.alias
+    all_legislation = Law.objects.using(db_alias).all()
+    for law in all_legislation:
+        law.classification = None
 
+    Law.objects.using(db_alias).bulk_update(all_legislation, ['classification'])
+
+def reverse_legislation_classifications(apps, schema_editor):
+    # TODO maybe something smart should happen, but we're probably fine
+    pass
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -28,6 +42,7 @@ class Migration(migrations.Migration):
             name='law',
             field=models.ForeignKey(blank=True, help_text='Piece of legislation this motion is about', null=True, on_delete=django.db.models.deletion.CASCADE, related_name='motions', to='parladata.law'),
         ),
+        migrations.RunPython(clean_legislation_classifications, reverse_legislation_classifications),
         migrations.AlterField(
             model_name='law',
             name='classification',
