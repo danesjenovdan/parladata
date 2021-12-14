10b4131276a3d8ac1ee257574f7d384a73026630
muki <filipdobranic@gmail.com>
2021-12-14 12:46:05 +0100

    fix crashing bug when a vote has no ballots

--- parladata/models/vote.py
+++ parladata/models/vote.py
@@ -103,7 +103,7 @@ class Vote(Timestampable, Taggable):
             * 'opposition': members which's not in coalition
         """
         option_counts = self.get_option_counts(gov_side)
-        if not option_counts:
+        if sum(option_counts.values()) == 0:
             return None
         max_option = max(option_counts, key=option_counts.get)
         max_percentage = option_counts.get(max_option, 0) / sum(option_counts.values()) * 100
