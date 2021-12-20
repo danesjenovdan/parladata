89bf4dcfd69f278f17da3cda5a6a6fe7a9b59f36
LepkoQQ <lepko.san@gmail.com>
2021-12-20 19:42:02 +0100

    remove agenda_items from speech admin

--- parladata/admin/admin.py
+++ parladata/admin/admin.py
@@ -54,7 +54,7 @@ class SpeechForm(forms.ModelForm):
 
 class SpeechAdmin(admin.ModelAdmin):
     form = SpeechForm
-    fields = ['session', 'content', 'speaker', 'order', 'motions', 'start_time', 'tags', 'lemmatized_content', 'agenda_items']
+    fields = ['session', 'content', 'speaker', 'order', 'motions', 'start_time', 'tags', 'lemmatized_content']
     list_filter = (SessionListFilter, 'tags')
     search_fields = ['speaker__name', 'content']
     autocomplete_fields = ['motions', 'speaker', 'session']
