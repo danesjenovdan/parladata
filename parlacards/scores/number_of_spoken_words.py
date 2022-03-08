362e9ff20575690ec91f0884705671be336af7c4
Filip Muki Dobranić <filipdobranic@gmail.com>
2022-03-08 09:41:53 +0100

    rookie mistake

--- parlacards/scores/number_of_spoken_words.py
+++ parlacards/scores/number_of_spoken_words.py
@@ -18,7 +18,10 @@ def calculate_number_of_spoken_words(speeches):
     return number_of_spoken_words
 
 
-def save_person_number_of_spoken_words(person, playing_field, timestamp=datetime.now()):
+def save_person_number_of_spoken_words(person, playing_field, timestamp=None):
+    if not timestamp:
+        timestamp = datetime.now()
+
     speeches = Speech.objects.filter_valid_speeches(
         timestamp
     ).filter(
@@ -35,16 +38,29 @@ def save_person_number_of_spoken_words(person, playing_field, timestamp=datetime
         playing_field=playing_field,
     ).save()
 
-def save_people_number_of_spoken_words(playing_field, timestamp=datetime.now()):
+def save_people_number_of_spoken_words(playing_field, timestamp=None):
+    if not timestamp:
+        timestamp = datetime.now()
+
     people = playing_field.query_voters(timestamp)
 
     for person in people:
         save_person_number_of_spoken_words(person, playing_field, timestamp)
 
-def save_people_number_of_spoken_words_between(playing_field, datetime_from=datetime.now(), datetime_to=datetime.now()):
+def save_people_number_of_spoken_words_between(playing_field, datetime_from=None, datetime_to=None):
+    if not datetime_from:
+        datetime_from = datetime.now()
+    if not datetime_to:
+        datetime_to = datetime.now()
+
     for day in get_dates_between(datetime_from, datetime_to):
         save_people_number_of_spoken_words(playing_field, timestamp=day)
 
-def save_sparse_people_number_of_spoken_words_between(playing_field, datetime_from=datetime.now(), datetime_to=datetime.now()):
+def save_sparse_people_number_of_spoken_words_between(playing_field, datetime_from=None, datetime_to=None):
+    if not datetime_from:
+        datetime_from = datetime.now()
+    if not datetime_to:
+        datetime_to = datetime.now()
+
     for day in get_fortnights_between(datetime_from, datetime_to):
         save_people_number_of_spoken_words(playing_field, timestamp=day)
