3cac00eba792fb97b317bfe33bc72704b58bc2f7
Tomaž <cofek0@gmail.com>
2022-03-24 19:01:48 +0100

    Update legislation.py
    slovenian parliament set legislation status

--- parladata/update_helpers/sl/legislation.py
+++ parladata/update_helpers/sl/legislation.py
@@ -1,29 +1,73 @@
 from parladata.models import Law, Motion, LegislationStatus
 
+from django.db.models import Q
 
-def contains_any(text, options):
-    for option in options:
-        if option in text:
-            return True
-    return False
+
+ENACTED = LegislationStatus.objects.get(name='enacted')
+ACCEPTED =  LegislationStatus.objects.get(name='submitted')
+REJECTED = LegislationStatus.objects.get(name='rejected')
 
 def set_legislation_result():
-    in_procedure = LegislationStatus.objects.get(name='in_procedure')
-    rejected = LegislationStatus.objects.get(name='rejected')
-    enacted = LegislationStatus.objects.get(name='enacted')
-    final_vote_titles = ['predlog Akta', 'predlog Odloka']
-    for legislation in Law.objects.filter(passed=None):
-        motions = legislation.motions.all()
-        for motion in motions:
-            if contains_any(motion.title, final_vote_titles):
-                if motion.result != None:
-                    legislation.passed = motion.result
-                    if motion.result:
-                        legislation.status = enacted
-                    else:
-                        legislation.status = rejected
-                    legislation.save()
+    for legislation in Law.objects.exclude(status__in=LegislationStatus.objects.filter(name__in=['rejected', 'enacted'])):
+        end_motion = legislation.motions.filter(
+            Q(title__icontains='v celoti') |
+            Q(title__icontains='ponovno odlo') |
+            Q(title__icontains='sklep mdt') |
+            Q(title__icontains='sklep o primernosti predloga zakona')
+        ).first()
+        print('legislation')
+        if end_motion:
+            print('has end motion')
+            set_single_legislation_result(legislation)
 
-        if legislation.status == None:
-            legislation.status = in_procedure
+def is_accepeted(motion):
+    accepted_option = False if 'ni primeren' in motion.text else True
+    return motion.result == accepted_option
+
+def set_single_legislation_result(legislation):
+    if legislation.text:
+        final_vote = Motion.objects.filter(law=legislation,
+                                           title__icontains='v celoti')
+        repeated_vote = Motion.objects.filter(law=legislation,
+                                              title__icontains='ponovno odlo')
+        mdt_vote = Motion.objects.filter(law=legislation,
+                                         title__icontains='sklep mdt')
+        beginning_vote = Motion.objects.filter(law=legislation,
+                                               title__icontains='sklep o primernosti predloga zakona')
+
+        print('legislation has text')
+        if final_vote:
+            if is_accepeted(final_vote[0]):
+                ## ENACTED
+                legislation.status = ENACTED
+                legislation.passed = True
+            else:
+                ## REJECTED
+                legislation.status = REJECTED
+                legislation.passed = False
             legislation.save()
+            return 'finished'
+
+        elif mdt_vote:
+            if is_accepeted(mdt_vote[0]):
+                ## REJECTED
+                legislation.status = REJECTED
+                legislation.passed = False
+                legislation.save()
+                return 'mdt'
+            else:
+                ## FURTHER VOTING WILL HAPPEN
+                pass
+
+
+        elif beginning_vote:
+            if not is_accepeted(beginning_vote[0]):
+                ## REJECTED
+                legislation.status = REJECTED
+                legislation.passed = False
+                legislation.save()
+                return 'beginning'
+            else:
+                ## FURTHER VOTING WILL HAPPEN
+                pass
+    return None
