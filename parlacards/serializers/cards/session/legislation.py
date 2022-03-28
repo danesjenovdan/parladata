7d2ea6101939cdb1feaca6321575fe39bb7b5cf3
LepkoQQ <lepko.san@gmail.com>
2022-03-28 18:33:04 +0200

    update session card serializer and relavant organizations for better filters

--- parlacards/serializers/cards/session/legislation.py
+++ parlacards/serializers/cards/session/legislation.py
@@ -6,7 +6,7 @@ from parlacards.serializers.legislation import LegislationSerializer
 
 class SessionLegislationCardSerializer(CardSerializer, LegislationMixin):
     def get_results(self, session):
-        # this is implemeted in to_representation for pagination
+        # this is implemented in to_representation for pagination
         return None
 
     def to_representation(self, session):
