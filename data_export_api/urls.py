e3bc8fd5c303b43ec008d6ce6de079f90f3ed77d
patricijab <patricija.brecko@gmail.com>
2022-03-28 09:41:05 +0200

    export votes and parliament members views

---
+++ data_export_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import *
+
+
+urlpatterns = [
+    path('export-votes', ExportVotesView.as_view()),
+    path('export-parliament-members', ExportParliamentMembersView.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'csv' ])
