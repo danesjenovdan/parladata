9e098bd0a80c950976301b6ba4e83e3ec1c6b992
patricijab <patricija.brecko@gmail.com>
2022-04-01 10:33:47 +0200

    create a base export resource view

--- export/views.py
+++ export/views.py
@@ -9,50 +9,42 @@ from rest_framework_csv import renderers as r
 from rest_framework.renderers import JSONRenderer, BaseRenderer
 
 
-class ExportVotesView(views.APIView):
+class ExportResourceView(views.APIView):
     """
-    Export all votes from database and return them as a file in one of the allowed formats (json, csv).
+    A view meant to be extended.
+    Exports resource objects and returns them as files.
     """
     renderer_classes = [r.CSVRenderer, JSONRenderer ]
+    filename = None
+    resource = None
 
     def get(self, request, format=None):
-        filename = "votes"
-
         if (format == "json"):
-            dataset_json = VoteResource().export_as_generator_json()
+            dataset_json = self.resource.export_as_generator_json()
             return StreamingHttpResponse(dataset_json, headers={
-                'Content-Disposition': f'attachment; filename="{filename}.json"'
+                'Content-Disposition': f'attachment; filename="{self.filename}.json"'
             })
-
         elif (format == "csv"):
-            dataset_csv = VoteResource().export_as_generator_csv()
+            dataset_csv = self.resource.export_as_generator_csv()
             return StreamingHttpResponse(dataset_csv, headers={
-                'Content-Disposition': f'attachment; filename="{filename}.csv"'
+                'Content-Disposition': f'attachment; filename="{self.filename}.csv"'
             })
         
         # format not supported
         return Response({'detail': 'format not supported'}, status=status.HTTP_404_NOT_FOUND)
 
 
-class ExportParliamentMembersView(views.APIView):
+class ExportVotesView(ExportResourceView):
+    """
+    Export all votes from database and return them as a file in one of the allowed formats (json, csv).
+    """
+    filename = "votes"
+    resource = VoteResource()
+
+
+class ExportParliamentMembersView(ExportResourceView):
     """
     Export all parliament members from database and return them as a file in one of the allowed formats (json, csv).
     """
-    renderer_classes = [r.CSVRenderer, JSONRenderer ]
-
-    def get(self, request, format=None):
-        filename = "parliament-members"
-
-        if (format == "json"):
-            dataset_json = MPResource().export_as_generator_json()
-            return StreamingHttpResponse(dataset_json, headers={
-                'Content-Disposition': f'attachment; filename="{filename}.json"'
-            })
-        elif (format == "csv"):
-            dataset_csv = MPResource().export_as_generator_csv()
-            return StreamingHttpResponse(dataset_csv, headers={
-                'Content-Disposition': f'attachment; filename="{filename}.csv"'
-            })
-
-        # format not supported
-        return Response({'detail': 'format not supported'}, status=status.HTTP_404_NOT_FOUND)
+    filename = "parliament-members"
+    resource = MPResource()
