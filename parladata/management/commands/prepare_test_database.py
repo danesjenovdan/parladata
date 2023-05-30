93949da8624f489740e0b087e6b56964fcb38f32
Tomaž kunst <cofek0@gmail.com>
2023-05-30 15:18:16 +0200

    fixed preparing  test database with bicameral systems

--- parladata/management/commands/prepare_test_database.py
+++ parladata/management/commands/prepare_test_database.py
@@ -6,6 +6,10 @@ from parladata.models.common import Mandate
 from parladata.models.vote import Vote
 from parladata.models.speech import Speech
 from parladata.models.memberships import PersonMembership, OrganizationMembership
+from parladata.models.versionable_properties import OrganizationName
+from datetime import datetime
+
+from parlacards.models import PersonNumberOfSpokenWords, GroupDiscord
 
 
 class Command(BaseCommand):
@@ -37,6 +41,10 @@ class Command(BaseCommand):
         PersonMembership.objects.filter(on_behalf_of=sd, mandate=mandate2).update(on_behalf_of=svoboda)
         OrganizationMembership.objects.filter(organization=mol2, member=sd).delete()
 
+        # fix set mandate to existing organization memberships
+        for root in OrganizationMembership.objects.filter(member__classification="root"):
+            OrganizationMembership.objects.filter(organization=root.member).update(mandate=root.mandate)
+
         Speech.objects.filter(session_id__in=[4280, 4283]).delete()
 
         # vote id 11094 contains only anonymous ballots
@@ -47,3 +55,64 @@ class Command(BaseCommand):
             ballot.save()
         # vote id 11092 contains no ballots at all
         Vote.objects.get(id=11092).ballots.all().delete()
+
+        # duplicate playing field (with session + data and memberships) for test bicameral systems
+        root_membership_mandate_2 = OrganizationMembership.objects.get(id=30)
+        root_membership_mandate_2.id = None
+        second_mol_2 = root_membership_mandate_2.member
+        second_mol_2.id = None
+        second_mol_2.save()
+        OrganizationName(
+                value='Mestni svet Dom 2 2022-2026',
+                owner=second_mol_2
+            ).save()
+        second_mol_2.save()
+        root_membership_mandate_2.member = second_mol_2
+        root_membership_mandate_2.save()
+
+        person_memberships = PersonMembership.objects.filter(mandate=mandate2, organization=mol2)
+        for membership in person_memberships:
+            membership.id = None
+            membership.save()
+            membership.organization = second_mol_2
+            membership.save()
+
+        sessions = Session.objects.filter(organizations=mol2)
+        for session in sessions:
+            motions = session.motions.all().defer(None)
+            speeches = session.speeches.all().defer(None)
+            questions = session.question_set.all().defer(None)
+            session.id = None
+            session.save()
+            session.organizations.clear()
+            session.organizations.add(second_mol_2)
+            session.save()
+            for motion in motions:
+                vote = motion.vote.first()
+                motion.id = None
+                motion.save()
+                motion.session = session
+                motion.save()
+                ballots = vote.ballots.all().defer(None)
+                vote.id = None
+                vote.save()
+                vote.motion = motion
+                vote.save()
+                for ballot in ballots:
+                    ballot.id = None
+                    ballot.save()
+                    ballot.vote = vote
+                    ballot.save()
+
+            for speech in speeches:
+                speech.id = None
+                speech.save()
+                speech.session = session
+                speech.save()
+
+            for question in questions:
+                question.id = None
+                question.save()
+                question.session = session
+                question.save()
+
