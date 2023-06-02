005da35195d74dd4dcf436feb1745b42ebe4e3b4
Tomaž kunst <cofek0@gmail.com>
2023-06-02 11:28:03 +0200

    added tests for bicameral systems

--- parladata/management/commands/prepare_test_database.py
+++ parladata/management/commands/prepare_test_database.py
@@ -5,8 +5,9 @@ from parladata.models.organization import Organization
 from parladata.models.common import Mandate
 from parladata.models.vote import Vote
 from parladata.models.speech import Speech
+from parladata.models.person import Person
 from parladata.models.memberships import PersonMembership, OrganizationMembership
-from parladata.models.versionable_properties import OrganizationName
+from parladata.models.versionable_properties import OrganizationName, PersonName
 from datetime import datetime
 
 from parlacards.models import PersonNumberOfSpokenWords, GroupDiscord
@@ -23,9 +24,10 @@ class Command(BaseCommand):
     * python manage.py migrate parlacards zero
     * python manage.py migrate
     * python manage.py run_all_analyses --start_time 2022-11-30
+    * python manage.py run_all_analyses --start_time 2022-12-13
     * python manage.py dumpdata -e contenttypes -e auth.permission -o tests/fixtures/test_db.json
     """
-
+    FAKE_PERSON_SOURCE_ID = 424
     def handle(self, *args, **options):
         # delete sessions
         Session.objects.all().exclude(id__in=[3783, 3782, 4280, 4283]).delete()
@@ -70,11 +72,46 @@ class Command(BaseCommand):
         root_membership_mandate_2.member = second_mol_2
         root_membership_mandate_2.save()
 
+        # fake person for testing bicameral systems
+        fake_primc = Person.objects.get(id=self.FAKE_PERSON_SOURCE_ID)
+        fake_primc.id = None
+        fake_primc.save()
+        PersonName(
+            value='Fake Primc',
+            owner=fake_primc
+        ).save()
+
+        # fake organization for testing bicameral systems
+        fake_vesna = Organization.objects.get(id=18)
+        fake_vesna.id = None
+        fake_vesna.save()
+        OrganizationName(
+            value='Fake vesna',
+            owner=fake_vesna
+        ).save()
+        original_vesna = Organization.objects.get(id=18)
+
+        org_memberships_mol2 = OrganizationMembership.objects.filter(
+            organization=mol2,
+        )
+        for org_mems in org_memberships_mol2:
+            if org_mems.member == original_vesna:
+                org_mems.member = fake_vesna
+                org_mems.id = None
+                org_mems.organization = second_mol_2
+                org_mems.save()
+
         person_memberships = PersonMembership.objects.filter(mandate=mandate2, organization=mol2)
         for membership in person_memberships:
             membership.id = None
-            membership.save()
             membership.organization = second_mol_2
+            if membership.member_id == self.FAKE_PERSON_SOURCE_ID:
+                membership.member = fake_primc
+                PersonMembership(member=fake_primc, organization=fake_vesna, start_time=membership.start_time).save()
+            if membership.on_behalf_of == original_vesna:
+                membership.on_behalf_of = fake_vesna
+            else:
+                membership.on_behalf_of = None
             membership.save()
 
         sessions = Session.objects.filter(organizations=mol2)
@@ -86,33 +123,34 @@ class Command(BaseCommand):
             session.save()
             session.organizations.clear()
             session.organizations.add(second_mol_2)
-            session.save()
             for motion in motions:
                 vote = motion.vote.first()
                 motion.id = None
-                motion.save()
                 motion.session = session
                 motion.save()
                 ballots = vote.ballots.all().defer(None)
                 vote.id = None
-                vote.save()
                 vote.motion = motion
                 vote.save()
                 for ballot in ballots:
                     ballot.id = None
-                    ballot.save()
                     ballot.vote = vote
                     ballot.save()
+                    if ballot.personvoter_id == self.FAKE_PERSON_SOURCE_ID:
+                        ballot.personvoter = fake_primc
+                        ballot.save()
 
             for speech in speeches:
                 speech.id = None
-                speech.save()
                 speech.session = session
                 speech.save()
+                if speech.speaker_id == self.FAKE_PERSON_SOURCE_ID:
+                        speech.speaker = fake_primc
+                        speech.save()
 
             for question in questions:
                 question.id = None
-                question.save()
                 question.session = session
                 question.save()
-
+                if question.person_authors == self.FAKE_PERSON_SOURCE_ID:
+                    question.person_authors.add(fake_primc)
