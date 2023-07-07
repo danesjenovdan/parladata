eaabcb8b3486c2e7b4b68038c5f922a4386beda4
Tomaž kunst <cofek0@gmail.com>
2023-07-07 11:46:35 +0200

    added mandate id to mandate serializer and fixed groups list

--- parlacards/serializers/mandate.py
+++ parlacards/serializers/mandate.py
@@ -5,5 +5,6 @@ from parladata.models.common import Mandate
 from parlacards.serializers.common import CommonSerializer
 
 class MandateSerializer(CommonSerializer):
+    id = serializers.IntegerField()
     description = serializers.TextField()
     beginning = serializers.DateTimeField()
